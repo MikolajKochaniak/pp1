7ba537294161669ca343b52055eb04f207b52c2c
MikolajKochaniak <kochaniakmikolaj@gmail.com>
2022-10-16 18:44:55 +0200

    Some new changes

---
+++ 02-TypesAndVariables/lekcja222.py
@@ -0,0 +1,27 @@
+Celcius = int(input("Ile stopni Celsiusza"))
+Fahrenheit = Celcius*9/5+32
+Kalvin = Celcius + 273,15
+print(f'Celcius = {Celcius}, Fahrenheit = {Fahrenheit}, Kalvin = {Kalvin}')
+
+print(5+10*5)
+print(3-2+1)
+print(2+-3)
+print(2**8)
+print(4+4/2**2)
+print(4%3%2%1)
+print(1+2%3**4*5)
+print(True != False)
+print(2<=3 or False)
+print(not True or not False and not True )
+print(2<3 and 4<5 or not 6<7)
+print(2%3<4/5 and 6+7<8 or not 9+10==19)
+print(0b11111>>1>>1>>1)
+print(0x11+0b11+11)
+print(2<<3>>4)
+
+
+centymetry = int(input("Jaki jest twój wzrost w centymetrach?"))
+feet = 0.0328*centymetry
+inche = 0.3937*centymetry
+print(f'I am {centymetry}cm i.e. {feet} feet and {inche} inches')
+
---
+++ 02-TypesAndVariables/lekcja_2_zad_20.py
@@ -0,0 +1,8 @@
+import random
+
+x = random.randint(1,6)
+y = random.randint(1,6)
+z = random.randint(1,6)
+
+print(f'pierwszy rzut  = {x} , drugi rzut = {y}, trzeci rzut = {z}')
+print(x+y+z)
