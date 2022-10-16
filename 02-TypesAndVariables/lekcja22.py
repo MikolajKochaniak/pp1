b7f874ae85ca4bd871a19477e3b12371d0712a7b
MikolajKochaniak <kochaniakmikolaj@gmail.com>
2022-10-16 18:33:59 +0200

    lekcja22.py added

---
+++ 02-TypesAndVariables/lekcja22.py
@@ -0,0 +1,26 @@
+
+
+
+x = int(input("x"))
+y = int(input("y"))
+print(x+y)
+
+name = input("name")
+age = input("age")
+height = input("height")
+print(f"My name is {name} , I'm {age} years old, and my height is {height} cm ")
+
+
+
+
+
+import math
+print(math.pi)
+r = 5
+Area = (r**2 * math.pi)
+circumference = (2*r * math.pi)
+print(Area)
+print(circumference)
+
+
+
