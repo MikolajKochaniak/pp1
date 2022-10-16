50dcd9abfcb0d9b02b4eb1098baf29b7639d80eb
MikolajKochaniak <kochaniakmikolaj@gmail.com>
2022-10-16 18:16:30 +0200

    Lesson 2: Name and surname problem

---
+++ 02-TypesAndVariables/lekcja 2.py
@@ -0,0 +1,37 @@
+from re import X
+
+
+print(3 - 2 + 1)
+print(5 + 10 * 5)
+print(4 + 4 / 2 ** 2)
+print(4 % 3 % 2 % 1)
+print(1 + 2 % 3 ** 4 * 5)
+True != False 
+
+n1 = 5
+n2 = 1
+n3 = 8 
+n4 = 6
+n5 = 3 
+print(n1 + n2 + n3 + n4 + n5)
+print(n1**2 + n2**2 + n3 **2 + n4**2 + n5**2)
+print(n3/n5)
+print(n3 == n4)
+
+x = 7
+y = 34
+z = y
+x = y
+x = z
+print(x)
+
+imie = input("Podaj imię : ")
+nazwisko = input("Podaj nazwisko : ")
+print(imie, nazwisko)
+print(f'{imie} {nazwisko}')
+
+
+
+
+
+
